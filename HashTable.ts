b64dfb3053b7497752f73ccf6efa7ebf7e224eab
Heemo Yang <heemo521@gmail.com>
2022-10-12 21:20:58 -0700

    Clean up

--- HashTable.ts
+++ HashTable.ts
@@ -1,1 +1,16 @@
-export class HashTable {}
+// export class HashTable {}
+
+//Hashing => key to index
+
+const word = 'areacode!';
+
+function firstFirstRep(str: string) {
+  const table = {};
+  for (const char of str) {
+    if (char in table) return char;
+    table[char] = 1;
+  }
+  return null;
+}
+
+console.log(firstFirstRep('areacode'));
