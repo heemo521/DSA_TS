1ac78fa4178a27591aca0844070c7d1060115f4e
Heemo Yang <heemo521@gmail.com>
2022-10-12 21:41:38 -0700

    Hashing Table set up

--- HashTable.ts
+++ HashTable.ts
@@ -1,18 +1,27 @@
-export class HashTable {}
+export class HashTable {
+  private _buckets: unknown[];
+  constructor() {
+    this._buckets = Array(100).fill(null);
+  }
+
+  set(value: string | number) {
+    this._buckets[Math.floor(Math.random() * 100)] = value;
+  }
+}
 
 //Hashing => key to index
 
-const word = 'I am Iron Man!';
+// const word = 'I am Iron Man!';
 
-function firstFirstRep(str: string) {
-  const table = new HashTable();
-  for (const char of str) {
-    if (char in table) return char;
-    table[char] = 1;
-  }
-  return null;
-}
+// function firstFirstRep(str: string) {
+//   const table = new HashTable();
+//   for (const char of str) {
+//     if (char in table) return char;
+//     table[char] = 1;
+//   }
+//   return null;
+// }
 
-const result = firstFirstRep(word);
+// const result = firstFirstRep(word);
 
-console.log('The expected result is "I" and the actual result is ' + result);
+// console.log('The expected result is "I" and the actual result is ' + result);
