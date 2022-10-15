68cf3ddec6dde5e0c99f723f00daaae088ccd660
Heemo Yang <heemo521@gmail.com>
2022-10-14 23:24:32 -0700

    Add priroity

--- PriorityQueue.ts
+++ PriorityQueue.ts
@@ -1,9 +1,11 @@
 export class Node {
   public value: unknown;
   public next: Node | null;
-  constructor(value: unknown) {
+  public priority: number;
+  constructor(value: unknown, priority: number) {
     this.value = value;
     this.next = null;
+    this.priority = priority;
   }
 }
 
@@ -14,17 +16,26 @@ class PriorityQueue {
     this.first = null;
   }
 
-  public insert(value: unknown) {
-    const newNode = new Node(value);
-    if (!this.first) {
+  public insert(value: unknown, priority: number) {
+    const newNode = new Node(value, priority);
+    if (!this.first || priority > this.first.priority) {
+      newNode.next = this.first;
       this.first = newNode;
     } else {
       let currentNode = this.first;
 
-      while (currentNode.next) {
+      while (currentNode.next && priority < currentNode.next.priority) {
         currentNode = currentNode.next;
       }
+      newNode.next = currentNode.next;
       currentNode.next = newNode;
     }
   }
 }
+
+const queue = new PriorityQueue();
+queue.insert('Clean up a room', 5);
+queue.insert('Do taxes', 99);
+queue.insert('Learn to code', 105);
+
+console.log(queue);
