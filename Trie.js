f793b8241edbda11f96adaa06ebf40e0f67db4b9
Heemo Yang <heemo521@gmail.com>
2022-10-14 21:18:50 -0700

    Trie Insert

---
+++ Trie.js
@@ -0,0 +1,36 @@
+'use strict';
+// exports.__esModule = true;
+// exports.Trie = exports.TrieNode = void 0;
+var TrieNode = /** @class */ (function () {
+  function TrieNode() {
+    this.value = null;
+    this.children = Array(26); // Creates an empty array with 26 available children
+  }
+  return TrieNode;
+})();
+// exports.TrieNode = TrieNode;
+var Trie = /** @class */ (function () {
+  function Trie() {
+    this.root = new TrieNode();
+  }
+  Trie.prototype.insert = function (key, value) {
+    var curNode = this.root;
+    for (var i = 0; i < key.length; i++) {
+      var alphabetIndex = key[i].charCodeAt(0) - 97; // Position of the character in the alphabet. 'a'.charCodeAt(0) - 97 will return 0 and 'z' will return 25
+      if (!curNode.children[alphabetIndex]) {
+        var newNode = new TrieNode();
+        curNode.children[alphabetIndex] = newNode;
+      }
+      curNode = curNode.children[alphabetIndex];
+    }
+    curNode.value = value;
+    return curNode;
+  };
+  return Trie;
+})();
+// exports.Trie = Trie;
+var trie = new Trie();
+trie.insert('a', 'apple');
+trie.insert('ass', [',ax', 'fsdf']);
+trie.insert('asking', 1234);
+console.log(trie);
--- Trie.ts
+++ Trie.ts
@@ -1,24 +1,39 @@
 export class TrieNode {
-  public value: string;
+  public value: null | unknown;
   public children: TrieNode[];
-  constructor(value: string) {
-    this.value = value;
-    this.children = Array(26);
+  constructor() {
+    this.value = null;
+    this.children = Array(26); // Creates an empty array with 26 available children
   }
 }
 
 export class Trie {
   public root: TrieNode;
   constructor() {
-    this.root = new TrieNode('');
+    this.root = new TrieNode();
   }
 
-  public insert(key, value) {
-    const root = this.root;
+  public insert(key: string, value: unknown) {
+    let curNode = this.root;
 
     for (let i = 0; i < key.length; i++) {
-        const alphabetIndex = key[i].charCodeAt(0) - 97;
-        if (!root.children[alphabetIndex])
+      const alphabetIndex = key[i].charCodeAt(0) - 97; // Position of the character in the alphabet. 'a'.charCodeAt(0) - 97 will return 0 and 'z' will return 25
+      if (!curNode.children[alphabetIndex]) {
+        const newNode = new TrieNode();
+        curNode.children[alphabetIndex] = newNode;
+      }
+      curNode = curNode.children[alphabetIndex];
     }
+    curNode.value = value;
+
+    return curNode;
   }
 }
+
+const trie = new Trie();
+
+trie.insert('a', 'apple');
+trie.insert('ass', [',ax', 'fsdf']);
+trie.insert('asking', 1234);
+
+console.log(trie);
