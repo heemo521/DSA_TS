7f4be4ed9b3db23c5416b150b4821154cb371b63
Heemo Yang <heemo521@gmail.com>
2022-10-15 18:28:47 -0700

    Methods for graphs

--- Graph.ts
+++ Graph.ts
@@ -1,25 +1,41 @@
 class Graph {
-  public nodes: { [key: string]: unknown } | {};
-  public edges: { id: number; list: number[] } | {};
+  private _nodes: { [key: number]: unknown };
+  private edges: { [key: number]: number[] };
 
   constructor() {
-    this.nodes = {};
+    this._nodes = {};
     this.edges = {};
   }
 
-  public addNode(identifier: string, value: unknown) {
-    this.nodes[identifier] = value;
+  public addNode(identifier: number, value: unknown) {
+    if (identifier in this._nodes) return null; // OR  delete this.edges[identifier];
+    this._nodes[identifier] = value;
   }
 
-  public addEdge(startNode: string, endNode: string) {
+  public addEdge(startNode: number, endNode: number) {
     if (
-      this.edges[startNode] &&
-      this.edges[startNode].indexOf(endNode) === -1
-    ) {
-      this.edges[startNode].push(endNode);
-      0;
-    } else {
-      this.edges[startNode] = [endNode];
-    }
+      !this._nodes[startNode] ||
+      !this._nodes[endNode] ||
+      startNode === endNode
+    )
+      return null;
+
+    // if there is an array of edges for the node
+    // if the endNode does not exists in the selected node's edges array
+    // Only then do we push in the endNode to the edges
+    if (this.edges[startNode]) {
+      if (this.edges[startNode].indexOf(endNode) === -1)
+        this.edges[startNode].push(endNode);
+      // The edges property does not exists for the given startNode so create one with endNode in it
+    } else this.edges[startNode] = [endNode];
+  }
+
+  public hasEdge(startNode: number, endNode: number) {
+    if (!this.edges[startNode]) return false;
+    return this.edges[startNode].indexOf(endNode) > -1;
+  }
+
+  public getAllEdges(node: number) {
+    return this.edges[node];
   }
 }
